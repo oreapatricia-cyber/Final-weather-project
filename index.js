6dde56ff37e769f05ab6c0309e8fe2e1790f4709
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-05 21:04:58 -0500

    c

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+function handleSearch(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
 let searchFormEelement = document.querySelector("#search-form");
 searchFormEelement.addEventListener("submit", handleSearch);

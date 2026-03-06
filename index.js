6755245ac208cac91babedad1735bdc0bb7b8615
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-05 21:23:55 -0500

    update

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+function searchCity(city) {
+  let apiKey = "d1b9c8a0e7f2c3b4e5f6g7h8i9j0k1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");

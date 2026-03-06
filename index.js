5452a7d30ac5627b2945a41799c5140e2aed7b38
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-05 21:15:41 -0500

    v

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
-  cityElement = searchInput.value;
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormEelement = document.querySelector("#search-form");

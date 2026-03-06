a1508ab4aa517f4ec6caead9becf976f20d019a2
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-05 22:04:06 -0500

    redo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function displayWeatherCondition(response) {
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+
   let temperature = response.data.temperature.current;
+
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
 }
@@ -8,15 +11,17 @@ function displayWeatherCondition(response) {
 function searchCity(city) {
   let apiKey = "94bo483a37241b0t66efb57f3864046e";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  // Call the API!
+  searchCity(searchInput.value);
 }
 
-let searchFormEelement = document.querySelector("#search-form");
-searchFormEelement.addEventListener("submit", handleSearch);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearch);

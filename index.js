a05ade9ebce2d99923edf0329ddb5b3e4d5fb2f0
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-08 13:54:15 -0400

    clean JS

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function displayWeatherCondition(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windspeedElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#weather-icon");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
@@ -15,41 +16,11 @@ function displayWeatherCondition(response) {
 
   let date = new Date(response.data.time * 1000);
   timeElement.innerHTML = formatDate(date);
+
+  // 🌤️ Dynamic icon update
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
-
-function searchCity(city) {
-  let apiKey = "94bo483a37241b0t66efb57f3864046e";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function formatDate(date) {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let day = days[date.getDay()];
-  let hours = date.getHours().toString().padStart(2, "0");
-  let minutes = date.getMinutes().toString().padStart(2, "0");
-
-  return `${day} ${hours}:${minutes}`;
-}
-
-function handleSearch(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  searchCity(searchInput.value);
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearch);
-
-// Load default city
-searchCity("Miami");

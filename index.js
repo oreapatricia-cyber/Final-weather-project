c0711a268b2ca45393ab226e648128e449181cea
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-08 13:35:57 -0400

    clean the JS

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ function displayWeatherCondition(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let timeElement = document.querySelector("#time");
-  let temperature = response.data.temperature.current;
-  let date = new Date(response.data.time * 1000);
-
-  temperatureElement.innerHTML = Math.round(temperature);
-  cityElement.innerHTML = response.data.city;
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.condition.description;
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   let windspeedElement = document.querySelector("#wind-speed");
-  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windspeedElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let date = new Date(response.data.time * 1000);
   timeElement.innerHTML = formatDate(date);
 }
+
 function searchCity(city) {
   let apiKey = "94bo483a37241b0t66efb57f3864046e";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -24,9 +25,6 @@ function searchCity(city) {
 }
 
 function formatDate(date) {
-  let day = days[date.getDay()];
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
   let days = [
     "Sunday",
     "Monday",
@@ -36,14 +34,22 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  return `${days[day]} ${hours}:${minutes}`;
+
+  let day = days[date.getDay()];
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+
+  return `${day} ${hours}:${minutes}`;
 }
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+// Load default city
+searchCity("Miami");

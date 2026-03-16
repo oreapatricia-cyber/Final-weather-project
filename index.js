c90677eae9faf3d95baf9d612ffb1203c0651e2c
oreapatricia-cyber <oreapatricia@gmail.com>
2026-03-16 18:46:16 -0400

    max

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function displayForecast(response) {
             class="weather-forecast-icon"
           />
           <div class="weather-forecast-temperature">
-            <strong>${Math.round(day.temperature.minimum)}°</strong>
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
           </div>
         </div>
       `;
